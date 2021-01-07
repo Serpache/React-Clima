c71303ed2f78f21ce81bea1be6817863638eeab2
Pache <serpache@ucm.es>
2021-01-07 23:44:34 +0100

    minor https fix

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     const consultarAPI = async () => {
       //Añadir aquí key de acceso en cuenta de openweathermap
       const appId = "code";
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
